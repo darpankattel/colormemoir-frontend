bbafa5f7cba88da6a0017d5597feba9dc885e281
Darpan <kattelktl@gmail.com>
2024-03-16 23:05:15 +0545

    changed api url

--- src/data/URL.tsx
+++ src/data/URL.tsx
@@ -1,2 +1,3 @@
-export const MAIN_URL = "https://8n8h2wz2-8000.inc1.devtunnels.ms";
+// export const MAIN_URL = "http://192.168.1.65:8000";
+export const MAIN_URL = "http://127.0.0.1:8000";
 export const URL = MAIN_URL + "/api/";
